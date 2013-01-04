654340378e420e27a419da63d3e315d0e170211f
Jeff Wear <jeff@inkling.com>
2013-01-04 03:59:20 -0800

    Test selector arguments using a dedicated method, not -performSelector:.
    -performSelector: is part of the NSObject protocol, to which NSProxy's
    conform. This means that when -performSelector:@selector(lowercaseString)
    is invoked on a mock or recorder, the invocation handled isn't
    for -performSelector", it is actually for -lowercaseString--they will try to
    actually perform the selector, and only record an invocation when they fail.

--- Source/OCMockTests/OCMockObjectTests.m
+++ Source/OCMockTests/OCMockObjectTests.m
@@ -25,6 +25,7 @@
 - (NSString *)method1;
 - (NSString *)method2;
 - (NSString *)method3:(NSString *)foo;
+- (void)method4:(SEL)foo;
 @end
 
 @implementation TestClassThatCallsSelf
@@ -42,6 +43,11 @@
 
 - (NSString *)method3:(NSString *)foo {
     return [foo uppercaseString];
+}
+
+- (void)method4:(SEL)foo
+{
+    
 }
 
 @end
@@ -647,9 +653,10 @@ static NSString *TestNotification = @"TestNotification";
 
 -(void)testAcceptsAndVerifiesMethodsWithSelectorArgument
 {
-	[[mock expect] performSelector:@selector(lowercaseString)];
-	[mock performSelector:@selector(lowercaseString)];
-	[mock verify];
+    id testMock = [OCMockObject mockForClass:[TestClassThatCallsSelf class]];
+    [[testMock expect] method4:@selector(lowercaseString)];
+    [testMock method4:@selector(lowercaseString)];
+    [testMock verify];
 }
 
 
