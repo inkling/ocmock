1bcc580df3c3105059ecf2cd99f004bfc82eb319
Jeff Wear <jeff@inkling.com>
2013-01-08 00:31:13 -0800

    Add documentation to OCPartialMockClassObject and the OCPartialMock protocol.

--- Source/OCMock/OCPartialMock.h
+++ Source/OCMock/OCPartialMock.h
@@ -11,10 +11,17 @@ static NSString *const OCMRealMethodAliasPrefix = @"ocmock_replaced_";
 
 @protocol OCPartialMock <NSObject>
 
+/** Returns the real object represented by the partial mock. */
 - (id)realObject;
 
+/** Directs the partial mock to stop mocking the real object. */
 - (void)stopMocking;
 
+/**
+ For internal use only:
+ Directs the partial mock to start handling invocations of the given selector
+ on the real object.
+ */
 - (void)setupForwarderForSelector:(SEL)selector;
 
 @end
--- Source/OCMock/OCPartialMockClassObject.h
+++ Source/OCMock/OCPartialMockClassObject.h
@@ -7,7 +7,13 @@
 
 #import "OCPartialMock.h"
 
-
+/**
+ OCPartialMockClassObject behaves and is used just like OCPartialMockObject, 
+ but for classes rather than instances of classes. It allows class methods 
+ to be partially mocked.
+ 
+ @warning A given class object can be partially-mocked by only one mock at a time.
+ */
 @interface OCPartialMockClassObject : OCMockClassObject <OCPartialMock>
 
 - (void)setupClass:(Class)aClass;
